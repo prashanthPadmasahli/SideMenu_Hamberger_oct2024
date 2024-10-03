1a8965ddd242282d787360e7d928425b1abce18f
prashanthPadmasahli <prashanthkumarshettigar@gmail.com>
2024-10-03 17:34:24 +0530

    Initial Commit

---
+++ SideMenu_Hamberger_oct2024/SideMenu_Hamberger_oct2024App.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenu_Hamberger_oct2024App.swift
+//  SideMenu_Hamberger_oct2024
+//
+//  Created by mac on 03/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SideMenu_Hamberger_oct2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
